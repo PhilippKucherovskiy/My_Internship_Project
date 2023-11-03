450cd26198ac9c089dd3871cdf01045cadc2460c
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-11-03 03:27:17 +0300

    Добавлена аутентификация

---
+++ My_Internship_Project/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using My_Internship_Project.Models;
+using My_Internship_Project.ViewModels;
+
+namespace My_Internship_Project.Controllers
+    {
+        public class AccountController : Controller
+        {
+            private readonly UserManager<User> _userManager;
+            private readonly SignInManager<User> _signInManager;
+
+            public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+            }
+
+            [HttpGet]
+            public IActionResult Login()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Login(LoginViewModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = await _userManager.FindByNameAsync(model.UserName);
+
+                    if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+                    {
+                        var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+
+                        if (result.Succeeded)
+                        {
+                           
+                            return RedirectToAction("INDEX", "Домашняя страница");
+                        }
+                    }
+
+                    ModelState.AddModelError(string.Empty, "Указан неверный логин");
+                }
+
+                return View(model);
+            }
+
+            public async Task<IActionResult> Logout()
+            {
+                await _signInManager.SignOutAsync();
+                return RedirectToAction("INDEX", "Домашняя страница");
+            }
+        }
+    }
+
+
---
+++ My_Internship_Project/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace My_Internship_Project.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+    }
+}
